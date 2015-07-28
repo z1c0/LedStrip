c63eeb46612477c21f6475340fefd9aefece42f9
z1c0 <wziegler@live.at>
2015-07-28 21:07:49 +0200

    Initial commit

---
+++ Arduino/LedServer/LedServer.ino
@@ -0,0 +1,74 @@
+#define FLOATEMIT 1
+#include <EtherCard.h>
+
+#define B_PIN 3
+#define R_PIN 5
+#define G_PIN 6
+
+static byte mymac[] = { 0x74, 0x69, 0x69, 0x2D, 0x30, 0x31 };
+static byte myip[] = { 192, 168, 1, 45 };
+
+byte Ethernet::buffer[500];
+BufferFiller bfill;
+
+const char HttpNotFound[] PROGMEM =
+  "HTTP/1.0 404 Unauthorized\r\n"
+  "Content-Type: text/html\r\n\r\n"
+  "<h1>404 Page Not Found</h1>";
+
+
+void setup ()
+{
+  if (ether.begin(sizeof Ethernet::buffer, mymac) == 0)
+  {
+    Serial.println("Failed to access Ethernet controller");
+  }
+  ether.staticSetup(myip);
+}
+
+static uint16_t GetTemperature()
+{
+  return (uint16_t)((analogRead(0) * 0.004882814 - 0.5) * 100.0);
+}
+
+static word HomePage(byte r, byte g, byte b)
+{
+  uint16_t temp = GetTemperature();
+  bfill = ether.tcpOffset();  
+  bfill.emit_p(PSTR(
+    "HTTP/1.0 200 OK\r\n"
+    "Content-Type: application/json\r\n"
+    "Pragma: no-cache\r\n"
+    "\r\n"
+    "{ 'red': $D, 'green': $D, 'blue': $D, 'temp': $D }"), r, g, b, temp);
+  return bfill.position();
+}
+
+int r = 0;
+int g = 0;
+int b = 0;
+
+void loop ()
+{
+  word len = ether.packetReceive();
+  word pos = ether.packetLoop(len);
+  if (pos)  // check if valid tcp data is received
+  {
+    bfill = ether.tcpOffset();
+    char* data = (char*) Ethernet::buffer + pos;
+    if (strncmp("GET /", data, 5) != 0)
+    {
+      bfill.emit_p(HttpNotFound);
+    }
+    else
+    {
+      data += 5;
+      sscanf(data, "?r=%d&g=%d&b=%d", &r, &g, &b);
+      HomePage(r, g, b);
+    }
+    ether.httpServerReply(bfill.position());    // send http response
+  }
+  analogWrite(B_PIN, b);
+  analogWrite(R_PIN, r);
+  analogWrite(G_PIN, g);
+}
---
+++ ledcontrol/public/javascripts/logic.js
@@ -0,0 +1,27 @@
+function switchColor(r, g, b) {
+  var query = "r=" + r + "&g=" + g + "&b=" + b;
+  $.ajax({
+    type: 'GET',
+    url: 'http://192.168.1.45/?' + query,
+  });
+}
+
+function makeTiles() {
+   var colors = [ 
+     [255, 255, 255],
+     [0, 0, 0],
+     [255, 0, 0],
+     [0, 255, 0],
+     [0, 0, 255],
+     [255, 255, 0],
+     [255, 128, 0],
+     [255, 0, 255], 
+     [0, 255, 255] 
+   ];
+   
+   colors.forEach(function(c) {	 
+      var r = c[0], g = c[1], b = c[2]; 
+      var html = "<a href='#' onclick='switchColor(" + r + ", " + g + ", " + b + "); return false;'><div class='tile' style='background-color: rgb(" + r + "," + g + "," + b + ")'></div></a>";
+      $("#container").append(html);
+   });
+}
