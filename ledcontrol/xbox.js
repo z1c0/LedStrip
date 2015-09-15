f8d50f4a0069a6f51db7907cb326d57a7aab16c5
z1c0 <wziegler@live.at>
2015-09-15 14:23:50 +0200

    Added time check for turning xbox off

--- ledcontrol/xbox.js
+++ ledcontrol/xbox.js
@@ -1,20 +1,24 @@
 var cron = require('cron'),
     http = require('http'),
+    moment = require('moment'),
     SsdpClient = require('node-ssdp').Client, 
     ssdpClient = new SsdpClient({
       //logLevel: 'TRACE',
       unicastHost: '192.168.1.146'
     });
-    
+
+var xboxLastSeen = moment();
 var xboxOn = false;
 
 // run every x seconds
-var cronJob = cron.job("*/20 * * * * *", function() {
+var cronJob = cron.job("*/5 * * * * *", function() {
   ssdpClient.search('ssdp:all');
-}); 
+});
 
-  ssdpClient.on('response', function (headers, statusCode, rinfo) {
+ssdpClient.on('response', function (headers, statusCode, rinfo) {
+  var secondsNotSeen = moment().diff(xboxLastSeen) / 1000;
   var h = "192.168.1.45";
+  
   var hour = new Date().getHours();
   //console.log(headers);
   //console.log(hour);
@@ -24,17 +28,18 @@ var cronJob = cron.job("*/20 * * * * *", function() {
       http.get({
         host: h,
         path: '/?r=0&g=255&b=0'}).on("error", function () { console.log("GET request error"); });
+      xboxLastSeen = moment();
       xboxOn = true;
     }
   } else {
-    if (xboxOn) { // add timeout
+    if (secondsNotSeen >= 30 && xboxOn) {
       console.log("XBox off");
       http.get({
         host: h,
         path: '/?r=0&g=0&b=0'}).on("error", function () { console.log("GET request error"); });
       xboxOn = false;
     }
-  }   
+  }
 });
 
 module.exports = {
